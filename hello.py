0f6dd9d8577557944c3add89268367e5bde34974
Tyler Darnell <tyler.a.darnell@gmail.com>
2013-05-11 16:27:45 -0500

    Working on getting list template functional.

--- hello.py
+++ hello.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 app = Flask(__name__)
+engine = create_engine("mysql://root:scavhunt@localhost/Scav")
+
+SessionMkr = sessionmaker()
+SessionMkr.configure(bind=engine)
+session = SessionMkr()
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/list')
+def list_reviews():
+    locations = session.query(Bathroom.location).all()
+    reviewsByLoc = {}
+    for location in locations:
+        reviewsByLoc[location] = session.query(Review).filter(Review.bathroom.location == location).all()
+
+    return render_template('list.html', locations=locations, reviews=reviewsByLoc)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=80)
+    app.run()

0ec0e1b1501753a24120ce6b98fdb4e1fe4324f8
Tyler Darnell <tyler.a.darnell@gmail.com>
2013-05-11 21:44:00 -0500

    Schema changed.

--- schema.sql
+++ schema.sql
@@ -39,7 +39,7 @@ DROP TABLE IF EXISTS `reviews`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `reviews` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `bathroom_id` int(11) DEFAULT NULL,
   `rating` int(11) DEFAULT NULL,
   `comment` varchar(1024) DEFAULT NULL,

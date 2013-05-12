992395de1c31f583e25a20beaa675c2068d1094e
Tyler Darnell <tyler.a.darnell@gmail.com>
2013-05-11 20:17:54 -0500

    Fixed some things, can't create reviews table? I dunno dudes.

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import models
+from models import Review, Bathroom
 
 app = Flask(__name__)
 engine = create_engine("mysql://root:scavhunt@localhost/Scav")
@@ -16,15 +16,16 @@ def index():
 
 @app.route('/list')
 def list_reviews():
-    locations = [instance.location for instance in session.query(models.Bathroom)]
+    locations = [instance.location for instance in session.query(Bathroom)]
     print locations
-    bathrooms = session.query(models.Bathroom).all()
+    bathrooms = session.query(Bathroom).all()
     print bathrooms
     return render_template('list.html', locs=locations, baths=bathrooms)
 
 @app.route('/bath/<bath>')
 def show_bath(bath):
-    pass
+    reviews = [instance for instance in session.query(Review).filter(Review.bathroom.id == bath)]
+    return render_template('bath.html', reviews=reviews)
 
 if __name__ == '__main__':
     app.run()
